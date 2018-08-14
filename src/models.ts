2a68084b220706262d27d1395f93762ee3bd30a6
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2018-08-14 22:57:55 +0200

    add compose and nest methods

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,3 @@
-import { AnyAction, Action } from 'redux'
+import { AnyAction, Action, Reducer } from 'redux'
 
-export type StatefulReducer<S = any, A extends Action = AnyAction> = (state: S, action: A) => S
-
-export type HigherOrderReducer<S = any, A extends Action = AnyAction> = (reducer: StatefulReducer<S, A>) => StatefulReducer<S, A>
+export type HigherOrderReducer<S = any, A extends Action = AnyAction> = (reducer: Reducer<S, A>) => Reducer<S, A>
