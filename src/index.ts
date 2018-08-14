700ebbb50bce1586389562acbadca706d4feaab9
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2018-08-14 22:57:55 +0200

    add compose and nest methods

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,73 @@
 
-import { HigherOrderReducer, StatefulReducer } from './models'
+import { HigherOrderReducer } from './models'
 import { Reducer, AnyAction, Action } from 'redux'
 
 export const initialState = <S = any, A extends Action = AnyAction>(initialState: S): Reducer<S, A> =>
   (state: S | undefined, action: A) => typeof state === 'undefined' ? initialState : state
 
-export const mergeState = <S = any, A extends Action = AnyAction>(newState: Partial<S>): StatefulReducer<S, A> =>
-  (state: S, action: A) => Object.assign({}, state, newState)
+export const identity = <S = any, A extends Action = AnyAction>(): Reducer<S, A> =>
+  (state: S | undefined, action: A) => {
+    if (typeof state === 'undefined') throw new Error('Undefined state passed to identity()')
+    return state
+  }
 
-export const wrap = <S = any, A extends Action = AnyAction>(outerReducer: Reducer<S, A>): HigherOrderReducer<S, A> =>
-  (innerReducer: StatefulReducer<S, A>) =>
-    (state: S, action: A) => outerReducer(innerReducer(state, action), action)
+export const always = <S = any, A extends Action = AnyAction>(alwaysState: S): Reducer<S, A> =>
+  (state: S | undefined, action: A) => alwaysState
 
-export const withActionType = <T, S = any, A extends Action<T> = AnyAction>(actionType: T, handler: Reducer<S, A>) =>
-  wrap<S, A>((state: S, action: A) => action.type === actionType ? handler(state, action) : state)
+// TODO: More generic, add tests
+export const composeReducers = <S = any, A extends Action = AnyAction>(
+  outerReducer: Reducer<S, A>,
+  innerReducer: Reducer<S, A>
+): Reducer<S, A> =>
+  (state: S, action: A) => outerReducer(innerReducer(state, action), action)
+
+export const elevate = <S = any, A extends Action = AnyAction>(
+  outerReducer: Reducer<S, A>
+): HigherOrderReducer<S, A> =>
+  (innerReducer: Reducer<S, A>) => composeReducers<S, A>(outerReducer, innerReducer)
+
+export const passState = <S = any, A extends Action = AnyAction>(): HigherOrderReducer<S, A> =>
+  elevate<S, A>(identity<S, A>())
+
+export const withState = <S = any, A extends Action = AnyAction>(newState: S): HigherOrderReducer<S, A> =>
+  elevate<S, A>(always<S, A>(newState))
+
+export const mergeState = <S = any, A extends Action = AnyAction>(newState: Partial<S>): HigherOrderReducer<S, A> =>
+  elevate<S, A>(state => Object.assign({}, state, newState))
+
+export const compose = <S = any, A extends Action = AnyAction>(
+  ...args: HigherOrderReducer<S, A>[]
+): HigherOrderReducer<S, A> =>
+  (innerReducer: Reducer<S, A>) =>
+    args.reduceRight(
+      (reducer: Reducer<S, A>, hor: HigherOrderReducer<S, A>) => hor(reducer),
+      innerReducer
+    )
+
+export const branch = <S = any, A extends Action = AnyAction>(
+  test: (state: S, action: A) => boolean,
+  left: HigherOrderReducer<S, A>,
+  right: HigherOrderReducer<S, A> = passState<S, A>()
+): HigherOrderReducer<S, A> =>
+  (innerReducer: Reducer<S, A>) =>
+    (state: S, action: A) => {
+      state = innerReducer(state, action)
+      return test(state, action) ? left(innerReducer)(state, action) : right(innerReducer)(state, action)
+    }
+
+export const withActionType = <T, S = any, A extends Action<T> = AnyAction>(
+  actionType: T,
+  actionHoR: HigherOrderReducer<S, A>
+): HigherOrderReducer<S, A> =>
+  branch<S, A>((state: S, action: A) => action.type === actionType, actionHoR)
+
+export const nest = <PS, S extends { [key: string]: any } = {}, A extends Action = AnyAction>(
+  propName: string,
+  propReducer: Reducer<PS, A>
+): HigherOrderReducer<S, A> =>
+  elevate<S, A>(
+    (state: S, action: A) =>
+      Object.assign({}, state, {
+        [propName]: propReducer(state[propName], action)
+      })
+  )
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import 'jest'
-import * as hor from '../src'
+import { initialState, mergeState, compose, branch, passState, withState, withActionType, nest, always, elevate, identity } from '../src'
 
 describe('initialState', () => {
   type State = {
@@ -7,15 +7,37 @@ describe('initialState', () => {
   }
 
   it('should set initial state when state is undefined', () => {
-    const reducer = hor.initialState<State>({ foo: 'bar' })
+    const reducer = initialState<State>({ foo: 'bar' })
     const newState = reducer(undefined, { type: 'FOO' })
     expect(newState).toEqual({ foo: 'bar' })
   })
 
   it('should pass state is it is not undefined', () => {
-    const reducer = hor.initialState<State>({ foo: 'bar' })
+    const reducer = initialState<State>({ foo: 'bar' })
     const newState = reducer({ foo: 'xxx' }, { type: 'FOO' })
     expect(newState).toEqual({ foo: 'xxx' })
+  })
+})
+
+describe('passState', () => {
+  it('should pass falsy state', () => {
+    const reducer = passState<boolean>()(initialState(false))
+    expect(reducer(undefined, { type: 'FOO' })).toBe(false)
+  })
+
+  it('should throw on undefined state', () => {
+    const reducer = passState<boolean>()(() => undefined)
+    expect(() => {
+      reducer(undefined, { type: 'FOO' })
+    }).toThrowError()
+  })
+})
+
+describe('withState', () => {
+  it('should overwrite state', () => {
+    const reducer = withState<number>(2)(initialState<number>(1))
+    const newState = reducer(undefined, { type: 'FOO' })
+    expect(newState).toBe(2)
   })
 })
 
@@ -26,12 +48,71 @@ describe('mergeState', () => {
   }
 
   it('should merge state correctly', () => {
-    const state = { foo: 'bar' }
-    const reducer = hor.mergeState<State>({ 'baz': 'qux' })
-    const newState = reducer(state, { type: 'FOO' })
+    const reducer = mergeState<State>({ 'baz': 'qux' })(initialState<State>({ foo: 'bar' }))
+    const newState = reducer(undefined, { type: 'FOO' })
     expect(newState).toEqual({
       foo: 'bar',
       baz: 'qux'
+    })
+  })
+})
+
+describe('compose', () => {
+  it('should compose 2 reducers', () => {
+    type State = {
+      aaa: string,
+      foo?: string,
+      bar?: string,
+      baz?: string
+    }
+    const reducer = compose<State>(
+      mergeState<State>({ foo: '111' }),
+      mergeState<State>({ bar: '222' }),
+      mergeState<State>({ baz: '333' })
+    )(initialState<State>({ aaa: 'bbb' }))
+    expect(reducer(undefined, { type: 'FOO' })).toEqual({
+      aaa: 'bbb',
+      foo: '111',
+      bar: '222',
+      baz: '333'
+    })
+  })
+})
+
+describe('branch', () => {
+  type State = {
+    foo?: string,
+    bar?: string
+  }
+
+  describe('both branches specified', () => {
+    const reducer = branch<number>(
+      (state, action) => action.type === 'FOO',
+      withState<number>(1),
+      withState<number>(2)
+    )(initialState(0))
+
+    it('should execute left branch is test returns truth', () => {
+      expect(reducer(undefined, { type: 'FOO' })).toBe(1)
+    })
+
+    it('should execute right branch is test returns truth', () => {
+      expect(reducer(undefined, { type: 'BAR' })).toBe(2)
+    })
+  })
+
+  describe('only left branch specified', () => {
+    const reducer = branch<number>(
+      (state, action) => action.type === 'FOO',
+      withState<number>(1)
+    )(initialState(0))
+
+    it('should execute left branch is test returns truth', () => {
+      expect(reducer(undefined, { type: 'FOO' })).toBe(1)
+    })
+
+    it('should pass the state if test returns false', () => {
+      expect(reducer(undefined, { type: 'BAR' })).toBe(0)
     })
   })
 })
@@ -43,8 +124,10 @@ describe('withActionType', () => {
   }
 
   it('should execute reducer when action matches', () => {
-    const initialsState = hor.initialState<State>({ baz: 'qux' })
-    const reducer = hor.withActionType<string, State>('FOO', hor.mergeState({ foo: 'bar' }))(initialsState)
+    const reducer = withActionType<string, State>(
+      'FOO',
+      mergeState({ foo: 'bar' })
+    )(initialState<State>({ baz: 'qux' }))
     const newState = reducer({ baz: 'qux' }, { type: 'FOO' })
     expect(newState).toEqual({
       foo: 'bar',
@@ -53,11 +136,63 @@ describe('withActionType', () => {
   })
 
   it('should not execute reducer when action doe not match', () => {
-    const initialsState = hor.initialState<State>({ baz: 'qux' })
-    const reducer = hor.withActionType<string, State>('FOO', hor.mergeState({ foo: 'bar' }))(initialsState)
+    const reducer = withActionType<string, State>(
+      'FOO',
+      mergeState({ foo: 'bar' })
+    )(initialState<State>({ baz: 'qux' }))
     const newState = reducer({ baz: 'qux' }, { type: 'BAR' })
     expect(newState).toEqual({
       baz: 'qux'
+    })
+  })
+})
+
+describe('nest', () => {
+  it('should call nested reducer and merge results', () => {
+    type State = {
+      foo: string
+    }
+    const reducer = nest<string, State>('foo', always('bar'))(initialState<State>({ foo: '' }))
+    expect(reducer(undefined, { type: 'FOO' })).toEqual({ foo: 'bar' })
+  })
+
+  it('should handle complex nesting', () => {
+    type State = {
+      foo: string
+      bar: number
+      baz: boolean
+    }
+    const reducer = compose(
+      nest<string, State>(
+        'foo',
+        withActionType('FOO', withState('yep'))(identity())
+      ),
+      nest<number, State>(
+        'bar',
+        withActionType('BAR', elevate<number>(state => state + 1))(identity())
+      ),
+      nest<boolean, State>(
+        'baz',
+        elevate<boolean>(state => !state)(identity())
+      )
+    )(initialState<State>({
+      foo: 'nope',
+      bar: 0,
+      baz: false
+    }))
+
+    const state1 = reducer(undefined, { type: 'FOO' })
+    expect(state1).toEqual({
+      foo: 'yep',
+      bar: 0,
+      baz: true
+    })
+
+    const state2 = reducer(reducer(undefined, { type: 'BAR' }), { type: 'BAR' })
+    expect(state2).toEqual({
+      foo: 'nope',
+      bar: 2,
+      baz: false
     })
   })
 })
